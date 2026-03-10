bafaafc85d228511d318d376c2d303e4332e1881
Vũ Tiến <ductienvu211103@gmail.com>
2026-03-10 09:11:11 +0700

    bttuan1

--- lessons/bai1_1/solution.ts
+++ lessons/bai1_1/solution.ts
@@ -7,7 +7,11 @@ export type Block = {
   current_hash: string;
 };
 
+import crypto from 'crypto';
+
 // ✍️ TODO: Viết hàm tại đây
 export function isValidBlock(block: Block): boolean {
-  return false; // Chỉnh lại logic
+  const data = block.index + block.timestamp + JSON.stringify(block.transactions) + block.previous_hash;
+  const calculatedHash = crypto.createHash('sha256').update(data).digest('hex');
+  return calculatedHash === block.current_hash;
 }
